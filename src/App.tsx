492ac884dacdb914a94cf8c2ca75a9cef545872e
Voragun Supawong <voragun2543@gmail.com>
2021-06-10 21:30:42 +0700

    [ADD] Login2

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="Username"/>
+        <br></br>
+        <input type="password" placeholder="Password" />
+        <br></br>
+        <button value="submit">Submit</button>
+      </form>
     </div>
   );
 }
